f5ec1603c44b7d95d784690d8b87f9e8373d2a6e
Null-Anomaly <lbryant019@insite.4cd.edu>
2025-11-14 20:08:13 -0800

    10 minutes

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*COMSC 210 | Lab 32 | Lawrence Bryant
+IDE used: VSC*/
+#include "Car.h"
+#include <deque>
+#include <stdlib.h>
+#include <random>
+#include <time.h>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    srand(time(NULL));
+
+    deque<Car> speed;
+
+    return 0;
+}
